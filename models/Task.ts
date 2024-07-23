dbe16299534d9ea27ef3359b35166fe21d6aa814
leobernier <33323385+leobernier@users.noreply.github.com>
2024-07-23 13:50:10 +0200

    add some data

--- models/Task.ts
+++
@@ -1,9 +0,0 @@
-interface Task {
-    id: number;
-    title: string;
-    description: string;
-    completed: boolean;
-    createdAt: Date;
-    // TODO: à implémenter dans le cadre du test
-    categories: any
-}
--- pages/index.vue
+++ pages/index.vue
@@ -3,33 +3,22 @@
     <div class="text-3xl font-bold mb-3">
       Todo List
     </div>
-    <TaskList :tasks="tasks" />
+
+    <ul>
+      <li
+        v-for="task in tasks"
+        :key="task.id"
+      >
+        {{ task.title }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-import TaskList from "~/components/TaskList.vue";
+import type { Task } from '~/types/Task'
 
-const tasks = ref([{
-  id: 1,
-  title: "Task 1",
-  description: "Description 1",
-  completed: false,
-  createdAt: new Date(),
-},
-{
-  id: 2,
-  title: "Task 2",
-  description: "Description 2",
-  completed: true,
-  createdAt: new Date(),
-},
-{
-  id: 3,
-  title: "Task 3",
-  description: "Description 3",
-  completed: false,
-  createdAt: new Date(),
-},
-])
+// L'endpoint se trouve dans le fichier `server/api/task.ts`
+const response = await useFetch<{ tasks: Task[] }>('/api/tasks')
+const tasks = response.data.value?.tasks || []
 </script>
